b29c9449b570c4ad4eb6e0afe15c392429687a0a
klaoude Yoloswag <neoklaoude@gmail.com>
2017-04-17 11:39:45 +0200

    board size

--- EchecV2/Board.h
+++ EchecV2/Board.h
@@ -5,7 +5,7 @@
 class Board
 {
 public:
-	Board(glm::vec2 position);
+	Board(glm::vec2 position, glm::vec2 size);
 	~Board();
 
 	void draw(KlaoudeEngine::SpriteBatch& spritebatch);
@@ -13,4 +13,5 @@ public:
 private:
 	GLuint m_textureID;
 	glm::vec2 m_position;
+	glm::vec2 m_size;
 };
